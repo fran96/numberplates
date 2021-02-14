3b6bbcaef2c5cc97ae4c2c9f492654907b57de9f
Ben <ben@popcore.com>
2021-02-14 21:46:26 +0100

    FE- purple horizontal

--- reactapp/src/views/NumberPlatesSearchView/NumberPlatesSearchView.js
+++ reactapp/src/views/NumberPlatesSearchView/NumberPlatesSearchView.js
@@ -5,101 +5,131 @@ import { Grid, Paper, CssBaseline, Container } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	paper: {
-		padding: theme.spacing(2),
-		textAlign: 'center',
-		boxShadow: "none",
-		backgroundColor: "transparent"
-	},
-	footer: {
-		[theme.breakpoints.down("xs")]: {
-			textAlign: "left",
-			marginLeft: "5px",
-		},
-		position: "fixed",
-		left: 0,
-		bottom: 0,
-		width: "100%",
-		textAlign: "center",
-	},
-	text: {
-		fontFamily: " Gilroy-Medium",
-		fontSize: "18px",
-		lineHeight: "21px",
-		letterSpacing: "0em",
-		textAlign: "right"
-
-	}
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    boxShadow: "none",
+    backgroundColor: "transparent",
+  },
+  footer: {
+    [theme.breakpoints.down("xs")]: {
+      textAlign: "left",
+      marginLeft: "5px",
+    },
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    width: "100%",
+    textAlign: "center",
+  },
+  text: {
+    fontFamily: "/fonts/Gilroy-Medium",
+    fontSize: "18px",
+    lineHeight: "21px",
+    letterSpacing: "0em",
+    textAlign: "right",
+  },
 }));
 
 const NumberPlatesSearchView = () => {
-	const classes = useStyles();
-	const [keyDown, setKeyDown] = useState(false);
-	const [searchTerm, setSearchTerm] = useState("");
+  const classes = useStyles();
+  const [keyDown, setKeyDown] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
-	let history = useHistory();
+  let history = useHistory();
 
-	useEffect(() => {
-		if (keyDown) {
-			history.push({
-				pathname: "/comments",
-				state: {
-					searchTerm: searchTerm,
-				},
-			});
-		}
-	}, [keyDown]);
-	return (
-		<div className={classes.root}>
+  useEffect(() => {
+    if (keyDown) {
+      history.push({
+        pathname: "/comments",
+        state: {
+          searchTerm: searchTerm,
+        },
+      });
+    }
+  }, [keyDown]);
+  return (
+    <div className={classes.root}>
+      <React.Fragment>
+        <Container maxWidth="sm">
+          <Grid
+            container
+            spacing={3}
+            alignItems="center"
+            style={{ backgroundColor: "#FFCF4D;", minHeight: "35vh" }}
+            justify="center"
+          >
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <img
+                  alt="Zvoga"
+                  src="/images/zvoga.png"
+                  width="179px"
+                  height="46px"
+                ></img>
+              </Paper>
+            </Grid>
+            <Grid item xs={6} style={{ marginTop: "15px" }}>
+              <Paper className={classes.paper}>
+                <div className={classes.text}>
+                  The new way to comment <br />
+                  on other people's driving!
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>
+                <img
+                  alt="Zvoga"
+                  src="/images/car-accident.png"
+                  width="83px"
+                  height="58px"
+                  margin-bottom="50px"
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+        <div
+          style={{ backgroundColor: "purple", height: "150px", width: "100%" }}
+        >
+          <Container maxWidth="sm">
+            <Grid
+              container
+              spacing={3}
+              alignItems="center"
+              justify="center"
+              style={{ paddingTop: "5%" }}
+            >
+              <Grid item xs={6}>
+                <div className={classes.text} style={{ color: "white" }}>
+                  Enter a license plate <br /> number to view and post <br />{" "}
+                  comments:
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <NumberPlatesSearch
+                  searchTerm={setSearchTerm}
+                  keyDown={setKeyDown}
+                />
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
 
-			<React.Fragment>
-				<CssBaseline />
-				<div style={{
-					height: "100px",
-					width: "800px",
-					left: "nullpx",
-					top: "nullpx",
-					borderRadius: "0px"
-				}}></div>
-				<Container maxWidth="sm">
-					<Grid container spacing={3} alignItems="center" style={{ backgroundColor: '#FFCF4D;', border: "6px solid black", minHeight: '70vh' }}
-						justify="center">
-						<Grid item xs={12}>
-							<Paper className={classes.paper}><img alt="Zvoga" src="/images/zvoga.png" width="179px" height="46px"></img></Paper>
-						</Grid>
-						<Grid item xs={6}>
-							<Paper className={classes.paper}>
-								<div className={classes.text}>The new way to comment on<br />other people's driving!</div></Paper>
-						</Grid>
-						<Grid item xs={6}>
-							<Paper className={classes.paper}><img alt="Zvoga" src="/images/car-accident.png" width="83px" height="58px" /></Paper>
-						</Grid>
-						<Grid item xs={3}>
-							<div className={classes.text}>
-								Enter a license plate <br /> number to view and post <br /> comments:
-							</div>
-						</Grid>
-						<Grid item xs={3}>
-							<NumberPlatesSearch searchTerm={setSearchTerm} keyDown={setKeyDown} /></Grid>
-						<Grid item >
-							<div className={classes.footer}>
-								<div>
-									<p style={{ color: "black" }}>
-										<b>Meet the creators</b>
-									</p>
-								</div>
-							</div>
-						</Grid>
-					</Grid>
-				</Container>
-			</React.Fragment>
-
-		</div>
-	);
+        <div className={classes.footer}>
+          <div>
+            <p style={{ color: "black" }}>
+              <b>Meet the creators</b>
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
+    </div>
+  );
 };
 
 export default NumberPlatesSearchView;
-
--- reactapp/src/views/NumberPlatesSearchView/components/NumberPlatesSearch/NumberPlatesSearch.js
+++ reactapp/src/views/NumberPlatesSearchView/components/NumberPlatesSearch/NumberPlatesSearch.js
@@ -5,55 +5,54 @@ import { makeStyles } from "@material-ui/styles";
 import { SearchInput } from "../../../../components";
 
 const useStyles = makeStyles((theme) => ({
-	root: {},
-	row: {
-		height: "60px",
-		display: "flex",
-		alignItems: "center",
-		[theme.breakpoints.down("xs")]: {
-			width: "50%",
-			margin: "0 auto",
-		},
-	},
-	spacer: {
-		flexGrow: 1,
-	},
-	importButton: {
-		marginRight: theme.spacing(1),
-	},
-	exportButton: {
-		marginRight: theme.spacing(1),
-	},
-	searchInput: {
-		margin: "0 auto",
-	},
+  // root: {},
+  // row: {
+  // 	height: "60px",
+  // 	display: "flex",
+  // 	alignItems: "center",
+  // 	[theme.breakpoints.down("xs")]: {
+  // 		width: "50%",
+  // 		margin: "0 auto",
+  // 	},
+  // },
+  // spacer: {
+  // 	flexGrow: 1,
+  // },
+  // importButton: {
+  // 	marginRight: theme.spacing(1),
+  // },
+  // exportButton: {
+  // 	marginRight: theme.spacing(1),
+  // },
+  // searchInput: {
+  // 	margin: "0 auto",
+  // },
 }));
 
 const NumberPlatesSearch = (props) => {
-	const { className, clientMessage, searchTerm, keyDown, ...rest } = props;
+  const { className, clientMessage, searchTerm, keyDown, ...rest } = props;
 
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<div {...rest} className={clsx(classes.root, className)}>
-			<div className={classes.row}>
-				<span className={classes.spacer} />
-			</div>
-			<div className={classes.row}>
-				<SearchInput
-					className={classes.searchInput}
-					placeholder=""
-					onChange={searchTerm}
-					keyDown={keyDown}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div {...rest} className={clsx(classes.root, className)}>
+      <div className={classes.row}>
+        <span className={classes.spacer} />
+      </div>
+      <div className={classes.row}>
+        <SearchInput
+          className={classes.searchInput}
+          placeholder=""
+          onChange={searchTerm}
+          keyDown={keyDown}
+        />
+      </div>
+    </div>
+  );
 };
 
 NumberPlatesSearch.propTypes = {
-	className: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default NumberPlatesSearch;
-
